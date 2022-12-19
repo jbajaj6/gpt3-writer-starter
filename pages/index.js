ccd133822a99af912017ab089d3584b860d9f5ee
jbajaj6 <jaibajaj06@gmail.com>
2022-12-19 18:49:15 -0500

    title

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>nextext</title>
       </Head>
       <div className="container">
         <div className="header">
