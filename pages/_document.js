fc5d491c6bca9dad99c5454c066fa7bda5e3bbb3
Jai Bajaj <jaibajaj06@gmail.com>
2022-12-20 13:04:30 -0500

    preview fixes

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="nextext" key="title"/>
+        <meta property="og:description" content="what should u text next?" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://miro.medium.com/max/1400/1*BVkgPgXLhrMl2o4DLT6-og.jpeg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
